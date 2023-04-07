01f6f0a0040ffa129228f3847c440377a39c5fbe
faizanhassan786 <hfaizan443@gmail.com>
2023-04-08 01:22:41 +0500

    craft converted to onnx format - following the official documentation

---
+++ inspecting-pytorch-models.py
@@ -0,0 +1,22 @@
+import torch.nn as nn
+
+class MyModel(nn.Module):
+    def __init__(self):
+        super(MyModel, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
+        self.relu2 = nn.ReLU(inplace=True)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu1(x)
+        x = self.conv2(x)
+        x = self.relu2(x)
+        x = self.pool(x)
+        return x
+
+model = MyModel()
+
+print("Model input requirement:", model.forward.__annotations__["x"])
